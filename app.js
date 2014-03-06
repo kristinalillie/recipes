f3ec78d8e5789873e2f5d0a3c0c75ed781c78d21
Kristina Lillie <kristinaalillie@gmail.com>
2014-03-05 23:53:48 -0800

    added content and markdown parser

--- app.js
+++ app.js
@@ -6,7 +6,9 @@
 var express = require('express');
 var http = require('http');
 var path = require('path');
-var recipes = require('./recipes/index.json')
+var fs = require('fs');
+var marked = require('marked');
+var recipes = require('./recipes/index.json');
 
 var app = express();
 
@@ -14,7 +16,7 @@ var app = express();
 app.set('port', process.env.PORT || 3000);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
-app.set('view options', {layout: false, root: path.join(__dirname, 'views')});
+app.set('view options', { layout: false, root: path.join(__dirname, 'views') });
 app.engine('html', require('ejs').renderFile);
 app.use(express.favicon());
 app.use(express.logger('dev'));
@@ -29,10 +31,22 @@ if ('development' == app.get('env')) {
   app.use(express.errorHandler());
 }
 
-app.get('/', function(req, res) {
-  res.render('index.html');
+app.get('/', function(req, res){
+  res.render('index.html', { recipes: recipes });
 });
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
 });
+
+function loadRecipes() {
+  for (var i = 0, n = recipes.length; i < n; i++){
+    var recipe = recipes[i]; 
+    var filePath = path.join('recipes', recipe.name + '.md');
+    if (!fs.existsSync(filePath)) continue;
+    var content = fs.readFileSync(filePath, 'utf-8');
+    recipe.content = marked(content);
+  }
+}
+
+loadRecipes();
